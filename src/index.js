f30c61403961df3c4d1bac44fd0af10d5af182c1
AkashBag2004 <akash.bag@iemlabs.com>
2025-02-13 18:15:54 +0530

    chnages

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
+import cors from "cors";
 const app = express();
 // const PORT = 5001;
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true,
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+//connection setup..
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
